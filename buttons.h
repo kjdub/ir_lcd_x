ac5fb2ba09104be72c359d4089c9d6e3d405462c
Kyle Wilson <kyle85@gmail.com>
2015-07-27 10:17:37 -0400

    seperate file for remote buttons

---
+++ buttons.h
@@ -0,0 +1,23 @@
+// define remote buttons :(
+#define CH 16736925
+#define CH_U 16769565
+#define CH_D 16753245
+#define NEXT 16712445
+#define PREV 16720605
+#define PLAY 16761405
+#define VOL_U 16754775
+#define VOL_D 16769055
+#define EQ 16748655
+#define ZERO 16738455
+#define HND1 16750695
+#define HND2 16756815
+#define ONE 16724175
+#define TWO 16718055
+#define THREE 16743045
+#define FOUR 16716015
+#define FIVE 16726215
+#define SIX 16734885
+#define SEVEN 16728765
+#define EIGHT 16730805
+#define NINE 16732845
+#define HOLD 4294967295 // received when any button is held on this remote
